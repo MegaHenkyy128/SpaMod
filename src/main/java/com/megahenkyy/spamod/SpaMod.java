561cea96fa0ebdbc33a4e82c30c1737a660cda44
Henky95 <henkkakkerlak@hotmail.com>
2017-04-02 17:34:30 +0200

    Started with the first steps

---
+++ src/main/java/com/megahenkyy/spamod/SpaMod.java
@@ -0,0 +1,20 @@
+package com.megahenkyy.spamod;
+
+import net.minecraftforge.fml.common.Mod;
+import scala.reflect.internal.Definitions;
+
+/**
+ * Created by Rutger on 2-4-2017.
+ */
+
+@Mod(
+        modid = "spamod",
+        name = "Spa Mod",
+        version = "$(version)"
+)
+public class SpaMod {
+    @Mod.Instance("spamod")
+    public static SpaMod instance;
+
+
+}
